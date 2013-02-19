eed231e0553476d762279d2e70e4003683054be8
Blair Anderson <blair81@gmail.com>
2013-02-18 18:09:58 -0700

    completed invoice tests for BI

--- test/sales_engine/invoice_test.rb
+++ test/sales_engine/invoice_test.rb
@@ -7,12 +7,21 @@ require './lib/sales_engine/invoice_loader'
 class InvoiceTest < MiniTest::Unit::TestCase
 
   def setup
+    CustomerLoader.from_csv("./test/data/short_customers.csv")
     InvoiceLoader.from_csv("./test/data/short_invoices.csv")
+    InvoiceItemLoader.from_csv("./test/data/short_invoice_items.csv")
+    ItemLoader.from_csv("./test/data/short_items.csv")
+    TransactionLoader.from_csv("./test/data/short_transactions.csv")
+    MerchantLoader.from_csv("./test/data/short_merchants.csv")
   end
 
   def teardown
-    a = Array.new()
-    Invoice.add(a)
+    Customer.add([])
+    Invoice.add([])
+    InvoiceItem.add([])
+    Item.add([])
+    Transaction.add([])
+    Merchant.add([])
   end
   
   def test_it_exists
@@ -60,61 +69,93 @@ class InvoiceTest < MiniTest::Unit::TestCase
     assert_equal "1", a.customer_id
   end
 
-  # def test_it_finds_all_by_customer_id
-  #   a = Invoice.find_all_by_customer_id("2")
-  #   puts a
-  # end
+  def test_it_finds_all_by_customer_id
+    a = Invoice.find_all_by_customer_id("1")
+    assert_equal "1", a.first.customer_id
+  end
 
   def test_it_find_by_merchant_id
     c = Invoice.find_by_merchant_id("26")
     assert_equal "26", c.merchant_id
   end
 
-  # def test_it_find_all_by_merchant_id
-  #   c = Invoice.find_all_by_merchant_id("27")
-  #   puts c
-  # end
+  def test_it_find_all_by_merchant_id
+    c = Invoice.find_all_by_merchant_id("26")
+    assert_equal "26", c.first.merchant_id
+  end
 
   def test_it_finds_by_status
     b = Invoice.find_by_status("shipped")
     assert_equal "shipped", b.status
   end
 
-  # def test_it_finds_all_by_status
-  #   b = Invoice.find_all_by_status("shipped")
-  #   puts b
-  # end
+  def test_it_finds_all_by_status
+    b = Invoice.find_all_by_status("shipped")
+    assert_equal "shipped", b.first.status
+  end
 
   def test_it_finds_by_created_at
     a = Invoice.find_by_created_at(Date.parse("2012-03-25 09:54:09 UTC"))
     assert_equal Date.parse("2012-03-25 09:54:09 UTC"), a.created_at
   end
 
-  #  def test_it_finds_all_by_created_at
-  #   a = Invoice.find_all_by_created_at("2012-03-07 12:54:10 UTC")
-  #   puts a
-  # end
+   def test_it_finds_all_by_created_at
+    a = Invoice.find_all_by_created_at(Date.parse("2012-03-25 09:54:09 UTC"))
+    assert_equal Date.parse("2012-03-25 09:54:09 UTC"), a.first.created_at
+  end
 
   def test_it_finds_by_updated_at
     a = Invoice.find_by_updated_at(Date.parse("2012-03-25 09:54:09 UTC"))
     assert_equal Date.parse("2012-03-25 09:54:09 UTC"), a.updated_at
   end
 
-  # def test_it_finds_all_by_updated_at
-  #   a = Invoice.find_all_by_updated_at("2012-03-07 12:54:10 UTC")
-  #   puts a
-  # end
-
-  def test_that_it_returns_a_transaction
-    skip "Our tests are broken but the method works"
+  def test_it_finds_all_by_updated_at
+    a = Invoice.find_all_by_updated_at(Date.parse("2012-03-25 09:54:09 UTC"))
+    assert_equal Date.parse("2012-03-25 09:54:09 UTC"), a.first.updated_at
   end
 
-  def test_that_it_returns_invoice_items
-    skip "Our tests are broken but the method works"
+  def test_it_finds_a_random_sample
+    a = Invoice.random
+    b = Invoice.random
+    c = Invoice.random
+    refute_equal a.id, b.id
+    refute_equal b.id, c.id
   end
 
-  def test_that_it_returns_customers
-    skip "Our tests are broken but the method works"
+  def test_it_returns_successful_invoices
+    a = Invoice.find_by_id("1")
+    assert_equal a.success?, true
+  end
+
+  def test_it_returns_pending_invoices
+    a = Invoice.find_by_id("1")
+    assert_equal a.pending?, false
+  end
+
+
+  def test_it_returns_invoice_items
+    a = Invoice.find_by_id("1")
+    assert_equal 8, a.invoice_items.count
+  end
+
+  def test_it_returns_invoice_revenue
+    a = Invoice.find_by_id("1")
+    assert_equal 21067, a.invoice_revenue.to_i
+  end
+
+  def test_it_returns_invoice_unit_quantity
+    a = Invoice.find_by_id("1")
+    assert_equal 47,a.invoice_unit_quantity
+  end
+
+  def test_it_returns_customer
+    a = Invoice.find_by_id("2")
+    assert_equal "1",a.customer.id
+  end
+
+  def test_it_returns_items_collection
+    a = Invoice.find_by_id("1")
+    assert_equal 8, a.items.count
   end
 
 end
